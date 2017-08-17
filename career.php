aba241eb16b1d2e03fd7ab852c602333d18d036e
A. K. M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2017-08-17 19:44:20 +0600

    tablet version menu adjust

--- career.php
+++ career.php
@@ -26,10 +26,10 @@
             <header id="header">
                 <div class="container">
                     <div class="row margin-top-bottom-10px">
-                        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-5">
+                        <div class="col-xs-12 col-sm-3 col-md-4 col-lg-5">
                             <a href="index.php"><img class="img-responsive margin-top-bottom-10px" src="images/logo.png" alt="Logo"></a>
                         </div>
-                        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-7">
+                        <div class="col-xs-12 col-sm-9 col-md-8 col-lg-7">
                             <nav class="navbar navbar-default header-navbar-custom margin-top-bottom-50px">
                                 <div class="navbar-header">
                                     <button type="button" class="navbar-toggle navbar-toggle-custom collapsed" data-toggle="collapse" data-target="#header-navbar-collapse" aria-expanded="false">
