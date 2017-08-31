70af2cffe6922a35d13ba9a81a24ae4c9618ef21
A. K. M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2017-08-31 08:58:32 +0600

    slider height related issue fixed

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
-<script type="text/javascript">
-    $(document).ready(function () {
-        //$('.carousel').carousel({interval: 3000});
-        $('#carouselFade').carousel();
-    });
-</script>
-<style>
-    #small-msg-box{
-        padding: 8px 32px!important; 
-    }
-    .large-msg-box-header{
-        padding: 3px 10px!important;  
-    }
-    .close-msg-box{
-        margin-top: 8px!important;  
-    }
-
-
-
-</style>
 <?php include './header.php'; ?>
 <body>
     <div class="wrapper">
@@ -57,21 +37,22 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <div id="carouselFade" class="carousel slide carousel-fade" data-ride="carousel">
-                        <div class="carousel-inner" role="listbox">
-                            <div class="item active"></div>
-                            <div class="item"></div>
-                            <div class="item"></div>
-                            <div class="item"></div>
+                    <div id="carouselFade" class="carousel slide carousel-fade">
+                        <div class="carousel-inner">
+                            <div class="item active"><img src="images/slide1.jpg" class="img-responsive carousel-img"></div>
+                            <div class="item"><img src="images/slide2.jpg" class="img-responsive carousel-img"></div>
+                            <div class="item"><img src="images/slide3.jpg" class="img-responsive carousel-img"></div>
+                            <div class="item"><img src="images/slide4.jpg" class="img-responsive carousel-img"></div>
                         </div>
-                        <!-- Indicators -->
+
+                        <!--Indicators--> 
                         <ol class="carousel-indicators">
                             <li data-target="#carouselFade" data-slide-to="0" class="active"></li>
                             <li data-target="#carouselFade" data-slide-to="1"></li>
                             <li data-target="#carouselFade" data-slide-to="2"></li>
                             <li data-target="#carouselFade" data-slide-to="3"></li>
                         </ol>
-                        <!-- Controls -->
+                        <!--Controls--> 
                         <a class="left carousel-control" href="#carouselFade" role="button" data-slide="prev">
                             <span class="tp-leftarrow " aria-hidden="true"></span>
                             <span class="sr-only"></span>
--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    $('#carouselFade').carousel({interval: 3000});
 //  wow js animation
     wow = new WOW(
             {
