42b3ae595d596a2103dd0b016cac588721167c7f
A. K. M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2017-10-22 17:31:36 +0600

    javaScript asynchronous error solve

--- header.php
+++ header.php
@@ -31,9 +31,9 @@
         <!--CSS ends-->
 
         <!--JS starts-->
-        <script type="text/javascript" src="js/jquery-1.12.0.min.js" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="js/wow.min.js" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="js/bootstrap.min.js" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="js/custom.min.js" crossorigin="anonymous"></script>
+        <script async type="text/javascript" src="js/jquery-1.12.0.min.js" crossorigin="anonymous"></script>
+        <script async type="text/javascript" src="js/wow.min.js" crossorigin="anonymous"></script>
+        <script async type="text/javascript" src="js/bootstrap.min.js" crossorigin="anonymous"></script>
+        <script async type="text/javascript" src="js/custom.min.js" crossorigin="anonymous"></script>
         <!--JS ends-->
     </head>
